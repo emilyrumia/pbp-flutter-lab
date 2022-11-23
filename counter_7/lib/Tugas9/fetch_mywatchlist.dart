eb00e4558f775bb9b42e5816c65bb78cd16aca86
Emily Rumia Naomi <emilyrumiaa@gmail.com>
2022-11-24 04:43:05 +0700

    Tugas 9

--- counter_7/lib/Tugas9/fetch_mywatchlist.dart
+++ counter_7/lib/Tugas9/fetch_mywatchlist.dart
@@ -13,10 +13,8 @@ Future<List<Movie>> fetchWatchList() async {
     },
   );
 
-  // melakukan decode response menjadi bentuk json
   var data = jsonDecode(utf8.decode(response.bodyBytes));
 
-  // melakukan konversi data json menjadi object WatchList
   List<Movie> listMovie = [];
   for (var d in data) {
     if (d != null) {
